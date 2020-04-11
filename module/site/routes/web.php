9143a96b1ab42d862eff4f6bb877a66026858eaa
Natanael Kristiawan <natanaelkristiawan@hotmail.com>
2020-04-11 22:21:37 +0800

    update

--- module/site/routes/web.php
+++ module/site/routes/web.php
@@ -1,4 +1,7 @@
 <?php
 $route->group(['middleware' => ['web']], function ($route) {
 	$route->get('', 'SiteResourceController@index')->name('public');
+  $route->get('login', function(){
+    
+  })->name('login');
 });
