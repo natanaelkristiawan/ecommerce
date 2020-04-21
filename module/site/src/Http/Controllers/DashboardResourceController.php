575a45c52c6b3c93cd7c2d8fce42288f698d0ba7
Natanael Kristiawan <natanaelkristiawan@hotmail.com>
2020-04-21 12:42:22 +0800

    update

--- module/site/src/Http/Controllers/DashboardResourceController.php
+++ module/site/src/Http/Controllers/DashboardResourceController.php
@@ -232,7 +232,7 @@ class DashboardResourceController extends Controller
 
         $download_link = '';
         if (!(bool)is_null($value->download_link)) {
-          $download_link = '<a download class="btn btn-sm btn-primary" href="'env('LINK_DOWNLOAD').$value->download_link.'" >Download</a>';
+          $download_link = '<a download class="btn btn-sm btn-primary" href="'.env('LINK_DOWNLOAD').$value->download_link.'" >Download</a>';
         } 
 
         $transfer_confirmation = '<a href="'.url('image/original/').'/'.$value->transfer_confirmation.'" data-featherlight="image"><img style="max-width:100px; display:block; margin:auto; border-radius:10px" class="img-fluid mb-2" alt="Responsive image" src="'.url('image/preview/').'/'.$value->transfer_confirmation.'"></img></a>';
