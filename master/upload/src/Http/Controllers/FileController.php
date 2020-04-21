90da9aa6d9027a7b3d76c6d0e13f54a425e8e467
Natanael Kristiawan <natanaelkristiawan@hotmail.com>
2020-04-21 13:08:23 +0800

    update

--- master/upload/src/Http/Controllers/FileController.php
+++ master/upload/src/Http/Controllers/FileController.php
@@ -16,9 +16,9 @@ class FileController extends Controller
     {
         $file_path = base_path(config('filer.folder')).'/'.str_replace('..', '', $path);
 
-        dd($file_path);
 
         if (file_exists($file_path) && is_file($file_path)) {
+            dd($file_path);
             // file found
             return response()->download($file_path);
         }
