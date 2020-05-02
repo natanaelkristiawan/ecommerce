603541370f48b5dd16a25696b720efe88c2c3eb3
Natanael Kristiawan <natanaelkristiawan@hotmail.com>
2020-05-02 20:58:19 +0800

    update

--- module/site/resources/views/dashboard/profile.blade.php
+++ module/site/resources/views/dashboard/profile.blade.php
@@ -99,35 +99,7 @@
           </form>
         </div>
       </div>
-
-
-      <div class="card">
-        <div class="card-header">
-          <div class="row">
-            
-          <div class="col-lg-6">
-            <h3 class="mb-0">Token Api</h3>
-          </div>     
-          <div class="col-lg-6 text-right">
-            <button class="btn btn-sm btn-danger" id="generateToken" type="button">Generate Token</button>
-          </div>
-          </div>
-        </div>
-        <div class="card-body">
-         <div class="form-group">
-            <label class="form-control-label">Public Key</label> 
-            <input readonly="" id="publicKey" value="{{ $data->public_key }}" type="text" placeholder="Public Key" class="form-control">
-          </div>
-          <div class="form-group">
-            <label class="form-control-label">Private Key</label> 
-            <input readonly type="text" id="privateKey" value="{{ $data->private_key }}" placeholder="Private Key" value=""class="form-control">
-          </div> 
-        </div>
-      </div>
     </div>
-
-
-
   <!-- Footer -->
   </div>
   @include('theme.admin.partials.copyright')
